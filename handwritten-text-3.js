21dff286c73a25825a1ee90a0e98cc2782394dd5
kosei-2025 <emwlgh39dell@gmail.com>
2025-02-03 18:14:40 +0900

    Update handwritten-text-3.js
    ズレを変更

--- handwritten-text-3.js
+++ handwritten-text-3.js
@@ -1,18 +1,18 @@
 class HandwrittenText extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
-      <svg width="700" height="200" viewBox="0 0 700 200" xmlns="http://www.w3.org/2000/svg">
+      <svg width="800" height="250" viewBox="0 0 800 250" xmlns="http://www.w3.org/2000/svg">
         <style>
           @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400&display=swap');
           
           text {
-            font-family: -apple-system, BlinkMacSystemFont, "San Francisco", "Roboto", sans-serif;
-            font-size: 50px;
+            font-family: 'Roboto', sans-serif;
+            font-size: 60px;
             fill: none;
             stroke: black;
             stroke-width: 2;
-            stroke-dasharray: 100;
-            stroke-dashoffset: 100;
+            stroke-dasharray: 120;
+            stroke-dashoffset: 120;
             animation: draw 3s forwards ease-in-out;
           }
 
@@ -41,31 +41,33 @@ class HandwrittenText extends HTMLElement {
           text:nth-child(16) { animation-delay: 1.6s; }
         </style>
         
-        <text x="20" y="100">A</text>
-        <text x="50" y="100">R</text>
-        <text x="80" y="100">C</text>
-        <text x="110" y="100">H</text>
-        <text x="140" y="100">I</text>
-        <text x="170" y="100">-</text>
-        <text x="200" y="100">P</text>
-        <text x="230" y="100">R</text>
-        <text x="260" y="100">I</text>
-        <text x="290" y="100">S</text>
-        <text x="320" y="100">M</text>
-        <text x="350" y="100">A</text>
+        <!-- 上の行（ARCHI-PRISMA） -->
+        <text x="30" y="100">A</text>
+        <text x="85" y="100">R</text>
+        <text x="140" y="100">C</text>
+        <text x="195" y="100">H</text>
+        <text x="250" y="100">I</text>
+        <text x="280" y="100">-</text>
+        <text x="320" y="100">P</text>
+        <text x="375" y="100">R</text>
+        <text x="430" y="100">I</text>
+        <text x="460" y="100">S</text>
+        <text x="515" y="100">M</text>
+        <text x="580" y="100">A</text>
 
-        <text x="20" y="160">D</text>
-        <text x="50" y="160">E</text>
-        <text x="80" y="160">Z</text>
-        <text x="110" y="160">I</text>
-        <text x="140" y="160">G</text>
-        <text x="170" y="160">N</text>
+        <!-- 下の行（DEZIGN WORKS） -->
+        <text x="30" y="180">D</text>
+        <text x="85" y="180">E</text>
+        <text x="140" y="180">Z</text>
+        <text x="195" y="180">I</text>
+        <text x="225" y="180">G</text>
+        <text x="290" y="180">N</text>
 
-        <text x="250" y="160">W</text>
-        <text x="290" y="160">O</text>
-        <text x="330" y="160">R</text>
-        <text x="370" y="160">K</text>
-        <text x="410" y="160">S</text>
+        <text x="400" y="180">W</text>
+        <text x="470" y="180">O</text>
+        <text x="530" y="180">R</text>
+        <text x="590" y="180">K</text>
+        <text x="650" y="180">S</text>
       </svg>
     `;
   }
